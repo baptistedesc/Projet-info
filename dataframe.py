ce0215fea2e1df8a273f96d25458c32625b948fe
HUGUUUUUUUUUES <105363486+HUGUUUUUUUUUES@users.noreply.github.com>
2022-05-14 19:12:16 +0200

    Add files via upload

--- dataframe.py
+++ dataframe.py
@@ -7,17 +7,64 @@ class dataframe:
         self.folder=folder
         self.filename=filename
     def chargement(self):
-        if 'csv.gz' in self.filename:
-            data = []
+        if '.csv.gz' in self.filename:
+            donnees = []
             with gzip.open(self.folder + self.filename, mode='rt') as gzfile :
                 synopreader = csv.reader(gzfile, delimiter=';')
                 for row in synopreader :
-                    data.append(row)
-        #Transformer ça en listes de listes
-        elif 'json.gz' in self.filename:
-            with gzip.open(self.folder + self.filename, mode='rt') as gzfile :
-                data = json.load(gzfile)
-        return data
+                    donnees.append(row)
+
+        elif '.json.gz' in self.filename:
+            with gzip.open(folder + filename, mode='rt', encoding='utf-8') as gzfile :
+                data0 = json.load(gzfile)
+            data1=[obs['fields'] for obs in data0]
+
+            nom_variables=[]
+            donnees=[]
+            n=len(data1)
+
+            #Création de la liste des variables
+            for i in range(n):
+                donnees.append([])
+                for cle in data1[i].keys():
+                    if not cle in nom_variables:
+                        nom_variables.append(cle)
+
+            #Assignation des valeurs manquantes
+            for i in range(n):
+                for j in range(len(nom_variables)):
+                    if not nom_variables[j] in data1[i].keys():
+                        data1[i][nom_variables[j]]='mq'
+
+            #Remplissage de la liste de liste données
+            for i in range(n):
+                for j in range(len(nom_variables)):
+                    donnees[i].append(data1[i][nom_variables[j]])
+
+            #Insertion de la liste des variables au début des données
+            donnees.insert(0,nom_variables)
+    
+        #On fait la transposée de la matrice des données en quelque sorte
+
+        dataframe=[]
+        for k in range(len(donnees[0])):
+            dataframe.append([])
+        for i in range(len(donnees[0])):
+            for j in range(len(donnees)):
+                dataframe[i].append(donnees[j][i])
+        return dataframe
+
+
+folder = '//filer-eleves.domensai.ecole/id1977/Projet-info/donnees_meteo/'
+filename = 'synop.201301.csv.gz'
+
+Frac=dataframe(folder,filename)
+print(dataframe.chargement(Frac)[0][0])
+
+
+
+
+
 
 
 
--- import_meteo.py
+++ import_meteo.py
@@ -2,12 +2,12 @@ import gzip
 import csv
 
 # Dossier où se trouve le fichier :
-folder = '//filer-eleves.domensai.ecole/id1977/traitement_donnees/'
-filename = 'synop.202203.csv.gz'
+folder = '//filer-eleves.domensai.ecole/id1977/Projet-info/donnees_meteo/'
+filename = 'synop.201301.csv.gz'
 data = []
 with gzip.open(folder + filename, mode='rt') as gzfile :
     synopreader = csv.reader(gzfile, delimiter=';')
     for row in synopreader :
         data.append(row)
 
-print(data)
+print(data[0])
