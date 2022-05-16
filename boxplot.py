6d986ef1a6b839535f0997ef85a6a31d79cfab26
Ruli0 <105363401+Ruli0@users.noreply.github.com>
2022-05-16 11:18:19 +0200

    Add files via upload
    Classe abstraite

---
+++ boxplot.py
@@ -0,0 +1,32 @@
+from cmath import tan
+from estimateur import Estimateur
+import matplotlib.pyplot as plt 
+import numpy as np
+
+
+class Boxplot(Estimateur):
+    def __init__(self,dataframe) -> None:
+        self.dataframe = dataframe
+    
+
+    def execute(self, tableau):
+        #Il faut créé un tableau adapté à la création d'un boxplot
+        #On prend en compte que le première élément de chaque liste est le nom de la colonne
+        nouveau_tableau = []
+        row = 0
+        col = 0
+        for i in range(0,len(tableau)):
+            row = row+1
+            for j in range(1,len(tableau[i])):
+                nouveau_tableau.append(tableau[i][j])
+                col = col+1
+        
+        print(col,row)
+        row = int(row)
+        col = int(col/row)
+        NewList = [nouveau_tableau[col*i : col*(i+1)] for i in range(row)]
+        print(NewList)
+        plt.boxplot(NewList)
+        plt.show()
+
+        
---
+++ estimateur.py
@@ -0,0 +1,7 @@
+from operation import Operation
+from abc import ABC, abstractmethod
+
+class Estimateur(Operation,ABC):
+    @abstractmethod
+    def execute():
+        pass
