733da12d0e6c846f6e9a51882c3823615d5c5557
faucon.hugues@gmail.com <faucon.hugues@gmail.com>
2022-05-28 01:19:55 +0200

    etsnhth

--- ptd/Transformations/jointure.py
+++ ptd/Transformations/jointure.py
@@ -36,37 +36,10 @@ class Jointure(Transformation):
             return table1
         elif type=='right':
             return Jointure.execute(table2,cle2,table1,cle1,'left')
-        elif type=='inner':
-            temporaire=[]
-            i,j=table1.nom_colonnes.index(cle1),table2.nom_colonnes.index(cle2)
-            temporaire=table2.valeurs[j]
-            del table2.nom_colonnes[j]
-            del table2.valeurs[j]
-            n=len(table1.valeurs)
-            table1.nom_colonnes=table1.nom_colonnes+table2.nom_colonnes
-            for u in range(len(table1.valeurs)-1):
-                table1.valeurs.append(['mq']*(len(table2.valeurs[j])))
-            for k in range(len(table1.valeurs[i])):
-                for p in range(len(temporaire)):
-                    if table1.valeurs[i][k]==temporaire[p]:
-                        for z in range(n,n+len(table2.valeurs)):
-                            table1.valeurs[z][k]=table2.valeurs[z-len(table1.valeurs)][p] 
-            print(temporaire)
-            print(table1.valeurs[i])
-            for k in range(len(table1.valeurs[i])):
-                t=0
-                for p in range(len(temporaire)):
-                    if table1.valeurs[i][k]==temporaire[p]:
-                        t=1
-                if t==0:
-                    for y in range(len(table1.valeurs)):
-                        del table1.valeurs[y][k]
 
-                    
-
-
-
-            return table1
+        # ça ne fonctionne pas, je ne sais pas du tout pourquoi
+        # elif type=='inner':
+        #     return Jointure.execute(Jointure.execute(table1,cle1,table2,cle2,'left'),cle1,table2,cle2,'right')
 
 
 
@@ -79,5 +52,5 @@ if __name__ == '__main__':
     valeurs2=[['8 ans','12 ans','7 ans'],['poire','pomme','abricot'],['heureux','pas heureux','moyen heureux']]
     test1=Table(nom_colonnes=colonnes1,valeurs=valeurs1)
     test2=Table(nom_colonnes=colonnes2,valeurs=valeurs2)
-    test3=Jointure.execute(test1,'fruits',test2,'fruits','inner')
+    test3=Jointure.execute(test1,'fruits',test2,'fruits','right')
     Table.afficher(test3)
