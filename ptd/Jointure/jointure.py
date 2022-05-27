e774166cca24f91122be2d25ea4bf2e8a1df8ab8
faucon.hugues@gmail.com <faucon.hugues@gmail.com>
2022-05-27 16:26:33 +0200

    grgr

---
+++ ptd/Jointure/jointure.py
@@ -0,0 +1,35 @@
+from ptd.table import Table
+
+class Jointure():
+    '''
+    Examples
+    -----------------
+
+    '''
+    def __init__(self, table1,cle1) :
+        '''
+        cle1=nom de la variable qui sert de clé
+        '''
+        self.table1=table1
+        self.cle1=cle1
+
+    def execute(self,table2,type):
+        self.table1.nom_colonnes.index(cle1)=j1
+        table2.nom_colonnes.index(cle2)=j2
+
+        nom_colonnes=self.table1.nom_colonnes+table2.nom_colonnes
+        
+        if type=='interne':
+
+
+        elif type=='gauche':
+
+        elif type=='droite':
+        
+        elif type=''
+  
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(verbose=True)
