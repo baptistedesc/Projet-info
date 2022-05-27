084d9bfb55906952a5d356612dcd2ee6b3e1ce6d
faucon.hugues@gmail.com <faucon.hugues@gmail.com>
2022-05-28 01:12:31 +0200

    thhre

--- ptd/Transformations/jointure.py
+++ ptd/Transformations/jointure.py
@@ -1,20 +1,12 @@
 from ptd.transformations.transformation import Transformation
-from ptd.transformations.selection import Selection
 from ptd.table import Table
 
 
 class Jointure(Transformation):
-    def __init__(self,table1) :
-        '''
-        Parameters
-        ----------
-        table1 : Table
-            Première table
-        
-        '''
-        self.table1=table1
+    def __init__(self) :
+        ''''''
 
-    def execute(self,cle1,table2,cle2):
+    def execute(table1,cle1,table2,cle2,type):
         '''Joint les deux tables 
 
         Parameters
@@ -26,47 +18,66 @@ class Jointure(Transformation):
         cle2 :
             Cle de la deuxième table
         '''
-        #Notations
-        i,j=self.table1.nom_colonnes.index(cle1),table2.nom_colonnes.index(cle2)
-        valeurs1,valeurs2=self.table1.valeurs,table2.valeurs
-        colonnes1,colonnes2=self.table1.nom_colonnes,table2.nom_colonnes
-        colonnes=colonnes1+colonnes2
-        #On ajoute des colonnes de valeurs manquantes à valeurs1
-        q=len(valeurs2[0])*['mq']
-        for u in range(len(colonnes2)-1):
-            valeurs1.append([q])
-        indices=[]
-        #On fait la jointure pour les valeurs communes des clés1 et clés2
-        for p in range(len(valeurs1[0])):
-            for k in range(len(valeurs2[0])):
-                if valeurs1[i][k]==valeurs2[j][p]:
-                    for x in range(len(colonnes1),len(colonnes1)+len(colonnes2)):
-                        if (x-len(colonnes1))!=j:
-                            valeurs1[x][k]=valeurs2[x-len(colonnes1)][p]
-                        pass
-            indices.append[p]
-        #On fait ensuite la jointure pour les valeurs non communes
-        for y in range(len(valeurs1)):
-            for s in range(len(valeurs2)):
-                for z in range(indices):
-                    valeurs1[i].append(valeurs2[j][z])
-                    if y<len(colonnes1) and y!=i:
-                        valeurs1[y].append('mq')
-                    elif s!=i:
-                         valeurs1[y].append(valeurs2[s][z])
-        del colonnes[len(self.table1.nom_colonnes)+i]
+        if type=='left':
+            temporaire=[]
+            i,j=table1.nom_colonnes.index(cle1),table2.nom_colonnes.index(cle2)
+            temporaire=table2.valeurs[j]
+            del table2.nom_colonnes[j]
+            del table2.valeurs[j]
+            n=len(table1.valeurs)
+            table1.nom_colonnes=table1.nom_colonnes+table2.nom_colonnes
+            for u in range(len(table1.valeurs)-1):
+                table1.valeurs.append(['mq']*(len(table2.valeurs[j])))
+            for k in range(len(table1.valeurs[i])):
+                for p in range(len(temporaire)):
+                    if table1.valeurs[i][k]==temporaire[p]:
+                        for z in range(n,n+len(table2.valeurs)):
+                            table1.valeurs[z][k]=table2.valeurs[z-len(table1.valeurs)][p] 
+            return table1
+        elif type=='right':
+            return Jointure.execute(table2,cle2,table1,cle1,'left')
+        elif type=='inner':
+            temporaire=[]
+            i,j=table1.nom_colonnes.index(cle1),table2.nom_colonnes.index(cle2)
+            temporaire=table2.valeurs[j]
+            del table2.nom_colonnes[j]
+            del table2.valeurs[j]
+            n=len(table1.valeurs)
+            table1.nom_colonnes=table1.nom_colonnes+table2.nom_colonnes
+            for u in range(len(table1.valeurs)-1):
+                table1.valeurs.append(['mq']*(len(table2.valeurs[j])))
+            for k in range(len(table1.valeurs[i])):
+                for p in range(len(temporaire)):
+                    if table1.valeurs[i][k]==temporaire[p]:
+                        for z in range(n,n+len(table2.valeurs)):
+                            table1.valeurs[z][k]=table2.valeurs[z-len(table1.valeurs)][p] 
+            print(temporaire)
+            print(table1.valeurs[i])
+            for k in range(len(table1.valeurs[i])):
+                t=0
+                for p in range(len(temporaire)):
+                    if table1.valeurs[i][k]==temporaire[p]:
+                        t=1
+                if t==0:
+                    for y in range(len(table1.valeurs)):
+                        del table1.valeurs[y][k]
 
-        return Table(colonnes,valeurs1)
-  
+                    
+
+
+
+            return table1
+
 
 
 if __name__ == '__main__':
     import doctest
     doctest.testmod(verbose=True)
-    colonnes=['fruits','âge']
-    valeurs1=[['pomme','poire'],['haricot','pois']]
-    valeurs2=[['pomme','poire'],['8 ans','12 ans']]
-    test1=Table(nom_colonnes=colonnes,valeurs=valeurs1)
-    test2=Table(nom_colonnes=colonnes,valeurs=valeurs2)
-    test3=Jointure.execute(test1,'fruits',test2,'fruits')
+    colonnes1=['fruits','legumes','sante']
+    colonnes2=['age','fruits','humeur']
+    valeurs1=[['kiwi','poire','abricot'],['haricot','pois','mq'],['bien','pas bien','moyen']]
+    valeurs2=[['8 ans','12 ans','7 ans'],['poire','pomme','abricot'],['heureux','pas heureux','moyen heureux']]
+    test1=Table(nom_colonnes=colonnes1,valeurs=valeurs1)
+    test2=Table(nom_colonnes=colonnes2,valeurs=valeurs2)
+    test3=Jointure.execute(test1,'fruits',test2,'fruits','inner')
     Table.afficher(test3)
