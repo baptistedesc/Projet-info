1e51324312ff40e98aa7f83b0f1b846bd06329ec
faucon.hugues@gmail.com <faucon.hugues@gmail.com>
2022-05-27 21:01:05 +0200

    rrg

--- ptd/Jointure/jointure.py
+++ ptd/Jointure/jointure.py
@@ -14,29 +14,34 @@ class Jointure():
         self.cle1=cle1
 
     def execute(self,table2,cle2,type):
-        assert self.cle1==cle2
-        self.table1.nom_colonnes.index(cle1)=j1
-        table2.nom_colonnes.index(cle2)=j2
         nom_colonnes=self.table1.nom_colonnes+table2.nom_colonnes
-        del nom_colonnes[len(self.table1.nom_colonnes+j2)]
-        
+        i,j=self.table1.nom_colonnes.index(self.cle1),table2.nom_colonnes.index(cle2)
+        valeurs1,valeurs2=self.table1.valeurs,table2.valeurs
+        colonnes1,colonnes2=self.table1.nom_colonnes,table2.nom_colonnes
+        nom_colonnes=colonnes1+colonnes2
+        q=len(valeurs2[0])*['mq']
+        for u in range(len(colonnes2)-1):
+            valeurs1.append([q])
+        indices=[]
+        for p in range(len(valeurs1[0])):
+            for k in range(len(valeurs2[0])):
+                if valeurs1[i][k]==valeurs2[j][p]:
+                    for x in range(len(colonnes1),len(colonnes1)+len(colonnes2)):
+                        if (x-len(colonnes1))!=j:
+                            valeurs1[x][k]=valeurs2[x-len(colonnes1)][k]
+                        pass
+            indices.append[p]
+        for y in range(len(valeurs1)):
+            for s in range(len(valeurs2)):
+                for z in range(indices):
+                    valeurs1[i].append(valeurs2[j][z])
+                    if y<len(colonnes1) and y!=i:
+                        valeurs1[y].append('mq')
+                    elif s!=i:
+                         valeurs1[y].append(valeurs2[s][z])
+        del nom_colonnes[len(self.table1.nom_colonnes)+i]
 
-
-        for i in range(len(self.table1.nom_colonnes)):
-            for j in range(len(table2.nom_colonnes)):
-                
-
-        if type=='interne':
-
-
-        elif type=='gauche':
-
-        elif type=='droite':
-        
-        elif type=''
-
-
-        return Table(nom_colonnes,valeurs2)
+        return Table(nom_colonnes,valeurs1)
   
 
 
